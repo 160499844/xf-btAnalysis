dbaa1d16db49696160dd3247b6fd5f058461b4f1
小峰 <160499844@qq.com>
2020-08-27 16:19:55 +0800

    增加种子信息查询

--- app/tests.py
+++ app/tests.py
@@ -9,22 +9,47 @@ def findAllFile(base):
         for filename in filelist:
             filenameEnd = os.path.splitext(filename)[-1]
             if(filenameEnd=='.torrent'):
-                print(os.path.join(path, filename))
+                #print(os.path.join(path, filename))
                 list.append(os.path.join(path, filename))
-        # Create your tests here.
+
+    return list
+
+
+
 def testSearchTorrentFile():
     """全盘搜索种子文件"""
-    base = os.walk("D:/")
+    base = os.walk("D:/迅雷下载")
     filesList = findAllFile(base)
+    for item in filesList:
+        #遍历每个种子文件
+        #print(item)
+        #组装信息
+        info = testGetInfo(item)
+        msg = info['msg']
+        if "ok" == msg:
+            data_created_by_ = info['data']['created_by']
+            data_creation_date_ = info['data']['creation_date']
+            data_files_ = info['data']['files']
+            data_name_ = info['data']['name']
+            data_length_ = info['data']['length']
+            print(data_created_by_)
+            print(data_creation_date_)
+            print(data_name_)
+            print(item)
+            print(data_length_)
+            print(data_files_)
+            print("---------------------------------------------------------")
+
+        pass
 
 
-def testGetInfo():
+def testGetInfo(filePath):
     """解析种子获取信息"""
     url = "https://api.toolnb.com/Tools/Api/btAnalysis.html"
     headers = {
         "origin":"https://www.toolnb.com",
         "referer":"https://www.toolnb.com/tools/torrentAnalysis.html",
     }
-    files = {"file":open("D:/迅雷下载/25F406F0C39CBDBE0936DC14E415C6AF45FDD793.torrent", "rb")}
+    files = {"file":open(filePath, "rb")}
     js = requests.post(url=url,headers=headers,files=files,data=None).json()
-    print(js)
+    return js
